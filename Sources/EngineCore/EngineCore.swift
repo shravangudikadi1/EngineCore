8cb89743356d4cede2e648465d51cc49c75d941a
shravangudikadi1 <94861850+shravangudikadi1@users.noreply.github.com>
2022-05-11 19:42:52 -0500

    sample text

--- Sources/EngineCore/EngineCore.swift
+++ Sources/EngineCore/EngineCore.swift
@@ -1,7 +1,14 @@
 import EngineUIKit
 import EngineCoreSwift
+import SwiftUI
+import Foundation
 
 public struct EngineCore {
     public var text = "Hello, World!"
     public init() {}
+    
+    @available(iOS 13.0.0, *)
+    public var body: some View {
+        Text("this is sample app ")
+    }
 }
